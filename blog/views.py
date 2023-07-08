18c0fa4482d43ee12e9ac9dce7b8e59c91806528
Khasan <info.kamalov@gmail.com>
2023-07-08 20:14:59 +0500

    New shop project

---
+++ blog/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .decorators import custom_login_required
+from .models import Category, New
+import datetime
+
+
+@custom_login_required
+def home(request):
+    category_id = request.GET.get('category_id')
+    news = New.objects.all().order_by('-id')
+    a_week_ago = datetime.date.today() - datetime.timedelta(days=7)
+    categories = Category.objects.all()
+    if category_id:
+        what_news = news.filter(category_id=category_id).order_by('-id')[:4]
+    else:
+        what_news = news.order_by('-id')[:4]
+    context = {
+        'last_new': news.first(),
+        "bottom_news": news[1:4],
+        'right_news': news[4:9],
+        "weekly_top_news": news.filter(created_at__gte=a_week_ago).order_by('-seen_qty'),
+        "categories": categories,
+        "what_news": what_news
+    }
+    return render(request, 'index.html', context)
+
+
+@custom_login_required
+def category(request):
+    return render(request, 'category.html')
+
+
---
+++ user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import custom_register, custom_login, custom_logout
+
+
+urlpatterns = [
+    path('register/', custom_register, name="register"),
+    path('login/', custom_login, name="login"),
+    path('logout/', custom_logout, name="logout"),
+]
